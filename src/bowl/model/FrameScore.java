389b319c4fa40042751ccd3c54bdcaa3c04345bb
Allan Sun <as4536@g.rit.edu>
2020-04-16 05:34:58 -0400

    Added comments to frame score class

--- src/bowl/model/FrameScore.java
+++ src/bowl/model/FrameScore.java
@@ -3,9 +3,9 @@ package bowl.model;
 import java.util.ArrayList;
 
 public class FrameScore implements IScore {
+    //List of the pins knocked down on each throw for the frame
     private ArrayList<Integer> ballThrows;
-    private boolean strike;
-    private boolean spare;
+    //Total amount of pins knocked down this frame
     private int score;
 
     public FrameScore() {
@@ -18,47 +18,36 @@ public class FrameScore implements IScore {
     }
 
     /**
-     * Adds to the score of this frame based on the pins downed during
-     * the next frame if this one was a strike or a spare. A strike means
-     * the total amount of pins knocked down on the next frame gets added
-     * to this frame's score. A spare means the number of pins knocked down
-     * on the first throw of the next frame gets added to this frame's score.
-     * @param nextFrame The FrameScore of the frame after this one.
+     * Add a number to the list of pins knocked down per throw, and
+     * add it to the total amount of downed pins for this frame (score).
+     * @param pinsDown The amount of pins knocked down.
      */
-    public void addScoreFromNextFrame(FrameScore nextFrame) {
-        if (this.strike) {
-            score += nextFrame.totalPinsDown();
-        }
-
-        if (this.spare) {
-            score += nextFrame.getBallThrows().get(0);
-        }
-    }
-
     public void newThrow(int pinsDown) {
         ballThrows.add(pinsDown);
         score += pinsDown;
-
-        if (totalPinsDown() == 10) {
-            if (ballThrows.size() == 1) {
-                strike = true;
-            } else if (ballThrows.size() == 2) {
-                spare = true;
-            }
-        }
     }
 
+    /**
+     * A strike has occurred for the frame if all 10 pins were knocked
+     * down in one throw.
+     * @return True if there was a strike, false otherwise.
+     */
     public boolean hasStrikeOccurred() {
-        return strike;
+        return totalPinsDown() == 10 && ballThrows.size() == 1;
     }
 
+    /**
+     * A spare has occurred for the frame if all 10 pins were knocked
+     * down in two throws.
+     * @return True if there was a strike, false otherwise.
+     */
     public boolean hasSpareOccurred() {
-        return spare;
+        return totalPinsDown() == 10 && ballThrows.size() == 2;
     }
 
     /**
      * Get the total number of pins knocked down for the frame
-     * @return
+     * @return The total number of pins downed
      */
     public int totalPinsDown() {
         int total = 0;
@@ -70,7 +59,29 @@ public class FrameScore implements IScore {
         return total;
     }
 
+    /**
+     * Get the array of number of pins knocked down per ball throw
+     * @return
+     */
     public ArrayList<Integer> getBallThrows() {
         return ballThrows;
+    }
+
+    /**
+     * Adds to the score of this frame based on the pins downed during
+     * the next frame if this one was a strike or a spare. A strike means
+     * the total amount of pins knocked down on the next frame gets added
+     * to this frame's score. A spare means the number of pins knocked down
+     * on the first throw of the next frame gets added to this frame's score.
+     * @param nextFrame The FrameScore of the frame after this one.
+     */
+    public void addScoreFromNextFrame(FrameScore nextFrame) {
+        if (this.hasStrikeOccurred()) {
+            score += nextFrame.totalPinsDown();
+        }
+
+        if (this.hasSpareOccurred()) {
+            score += nextFrame.getBallThrows().get(0);
+        }
     }
 }
